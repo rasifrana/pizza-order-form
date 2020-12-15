7bf634b7e5ec8f3ed32ed85471f30da9facf5da1
Asif Rana <muhammadasif@Muhammads-MacBook-Pro.local>
2020-12-15 22:54:48 +0000

    validation for password input

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-let size = document.getElementById("size");
-let quantity = document.getElementById("quantity");
-let total = document.querySelector("#amount");
-let discountDiv = document.querySelector(".discount");
-let discountBtn = document.querySelector("#discount-btn");
-let discountCode = document.querySelector("#discount-code");
+let size = document.getElementById("size");   //Selet size of pizza
+let quantity = document.getElementById("quantity");  // Quantity
+let total = document.querySelector("#amount");  //Total price
+let discountDiv = document.querySelector(".discount");  // Discount Div 
+let discountBtn = document.querySelector("#discount-btn");  // button for discount
+let discountCode = document.querySelector("#discount-code"); //input for code to apply
 
 var originalPrice = 0;
 let quantityTotal = 0;
 let finalAmount = 0;
 
+
+//  This function wil check the size of pizza and set price total
 size.addEventListener("change", function(e){
     if(size.value == "no") {
-        quantity.setAttribute("disabled", "disabled");
+        quantity.setAttribute("disabled", "disabled");  // if there is no size selection, quantity is disabled
     } else {
         quantity.removeAttribute("disabled", "disabled");
     }
@@ -21,6 +23,8 @@ size.addEventListener("change", function(e){
     total.innerHTML = finalAmount;
 });
 
+
+//  This function wil check the quantity of pizza and set price total
 quantity.addEventListener("change", (e)=>{
     let qt = Number(e.target.value);
     quantityTotal  = qt;
@@ -28,29 +32,58 @@ quantity.addEventListener("change", (e)=>{
     total.innerHTML = finalAmount;
 });
 
+//  This function will apply Discount
 discountBtn.addEventListener("click", function(e){
     e.preventDefault();
     let discValue = discountCode.value;
     if (discValue === ""){
-        discountDiv.innerHTML = `<h3 class="discount-error">Please Enter Coupon</h3>`
+        discountDiv.innerHTML = `<p class="discount-error">Please Enter Discount Code</p>`
     } else {
-        if(  discValue ==="asif"){
-            let discountedAmount = finalAmount * .2 ;
+        if(  discValue ==="extracheese"){
+            let discountedAmount = finalAmount * .1 ;
 
             let afterDiscount = finalAmount - discountedAmount;
             
             total.innerHTML = afterDiscount;
-            discountDiv.innerHTML = `<h3 class="discount-success">Successfully applied 20% Discount</h3>`
+            discountDiv.innerHTML = `<p class="discount-success">Successfully applied 10% Discount</p>`
             
         } else {
-            discountDiv.innerHTML = `<h3>Wrong Answer</h3>`
+            discountDiv.innerHTML = `<p class="discount-error">Please Enter Valid Code</p>`
         }
-        
     }
-    
-    
 });
 
 if(size.value == "no") {
     quantity.setAttribute("disabled", "disabled");
+}
+
+$(document).ready(function(){
+    $('#password').keyup(function(){
+        passwordValidation();
+    });
+
+    $('#size').change(function(){
+        extracheeseMsg();
+    });
+
+});
+
+// Password Validation
+function passwordValidation(){
+    let password = $('#password').val();
+    if (password.length > 7) {
+        $('.error-msg').text("Maximum 7 Character Allowed");
+    } else {
+        $('.error-msg').text("");
+    }
+}
+
+// Message to show they selected supersize
+function extracheeseMsg(){
+    let sizeVal = $('#size').val();
+    if (sizeVal == "17") {
+        $('.supersize-msg').text("High Calories Product");
+    } else {
+        $('.supersize-msg').text("");
+    }
 }
