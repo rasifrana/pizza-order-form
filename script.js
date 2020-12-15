6d20117f826ad570448a4275797f5a47334c1add
Asif Rana <muhammadasif@Muhammads-MacBook-Pro.local>
2020-12-15 17:42:41 +0000

    apply coupon added

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 let size = document.getElementById("size");
-
 let quantity = document.getElementById("quantity");
+let total = document.querySelector("#amount");
 
-console.log(quantity.value);
+
+var originalPrice = 0;
+let quantityTotal = 0;
+
+size.addEventListener("change", function(e){
+    var price = Number(e.target.value);
+    originalPrice = price;
+});
+
+quantity.addEventListener("change", (e)=>{
+    let qt = Number(e.target.value);
+    quantityTotal  = qt;
+    let finalAmount = quantityTotal * originalPrice;
+    total.innerHTML = finalAmount;
+});
+
+
